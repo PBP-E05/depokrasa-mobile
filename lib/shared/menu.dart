b28ec2d06d66f52218ff596088083d9d806672b0
zerothy <zxuyagaming@gmail.com>
2024-12-09 04:39:03 +0700

    feat: a little design for news

--- lib/shared/menu.dart
+++ lib/shared/menu.dart
@@ -67,128 +67,182 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+        backgroundColor: Colors.yellow.shade700,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (widget.user.isAdmin)
-              TextButton(
-                onPressed: () async {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AddNewsPage(
-                        user: widget.user,
-                        onNewsSubmitted: fetchFeaturedNews, // Pass the callback
-                      ),
-                    ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  minimumSize: const Size(100, 50),
-                  backgroundColor: Colors.yellow.shade700,
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-                child: const Text('Add News'),
-              ),
-            const SizedBox(height: 20),
-            if (featuredNewsList.isNotEmpty)
-              CarouselSlider.builder(
-                carouselController: _carouselController,
-                itemCount: featuredNewsList.length,
-                options: CarouselOptions(
-                  height: 400.0,
-                  enlargeCenterPage: true,
-                  autoPlay: true,
-                  aspectRatio: 16 / 9,
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  enableInfiniteScroll: false,
-                  autoPlayInterval: const Duration(seconds: 3),
-                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                  viewportFraction: 0.8,
-                ),
-                itemBuilder: (context, index, realIndex) {
-                  final news = featuredNewsList[index];
-                  return Stack(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.2),
-                              spreadRadius: 3,
-                              blurRadius: 10,
-                              offset: const Offset(0, 6),
-                            ),
-                          ],
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.asset(
-                            'images/image1.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      if (widget.user.isAdmin)
-                        Positioned(
-                          top: 10,
-                          right: 10,
-                          child: Row(
-                            children: [
-                              IconButton(
-                                icon: const Icon(Icons.edit, color: Colors.white),
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => EditNewsPage(
-                                        user: widget.user,
-                                        news: news,
-                                        onNewsUpdated: fetchFeaturedNews,
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
-                              IconButton(
-                                icon: const Icon(Icons.delete, color: Colors.white),
-                                onPressed: () async {
-                                  await deleteNews(news.id);
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                    ],
-                  );
-                },
-              )
-            else
-              const CircularProgressIndicator(),
-            const SizedBox(height: 20),
-            Row(
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  onPressed: () => _carouselController.previousPage(),
-                  child: const Icon(Icons.arrow_back),
-                ),
-                const SizedBox(width: 20),
-                ElevatedButton(
-                  onPressed: () => _carouselController.nextPage(),
-                  child: const Icon(Icons.arrow_forward),
-                ),
+                if (widget.user.isAdmin)
+                  TextButton(
+                    onPressed: () async {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AddNewsPage(
+                            user: widget.user,
+                            onNewsSubmitted: fetchFeaturedNews, // Pass the callback
+                          ),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      minimumSize: const Size(100, 50),
+                      backgroundColor: Colors.yellow.shade700,
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    child: const Text('Add News'),
+                  ),
+                const SizedBox(height: 20),
+                if (featuredNewsList.isNotEmpty)
+                  CarouselSlider.builder(
+                    carouselController: _carouselController,
+                    itemCount: featuredNewsList.length,
+                    options: CarouselOptions(
+                      height: 400.0,
+                      enlargeCenterPage: true,
+                      autoPlay: true,
+                      aspectRatio: 16 / 9,
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      enableInfiniteScroll: false,
+                      autoPlayInterval: const Duration(seconds: 3),
+                      autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                      viewportFraction: 0.8,
+                    ),
+                    itemBuilder: (context, index, realIndex) {
+                      final news = featuredNewsList[index];
+                      return Stack(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 10),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(color: Colors.black, width: 2),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.white,
+                                  spreadRadius: 3,
+                                  blurRadius: 10,
+                                  offset: const Offset(0, 6),
+                                ),
+                              ],
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.asset(
+                                    'images/image1.jpg',
+                                    fit: BoxFit.cover,
+                                    height: 200,
+                                    width: double.infinity,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          news.title,
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.yellow.shade700,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 5),
+                                        Text(
+                                          news.content,
+                                          maxLines: 3,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.grey.shade700,
+                                          ),
+                                        ),
+                                        const SizedBox(height: 10),
+                                        Text(
+                                          'Published on: ${news.timeAdded}',
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color: Colors.grey.shade600,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          if (widget.user.isAdmin)
+                            Positioned(
+                              top: 10,
+                              right: 10,
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                    icon: const Icon(Icons.edit, color: Colors.white),
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => EditNewsPage(
+                                            user: widget.user,
+                                            news: news,
+                                            onNewsUpdated: fetchFeaturedNews,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                  IconButton(
+                                    icon: const Icon(Icons.delete, color: Colors.white),
+                                    onPressed: () async {
+                                      await deleteNews(news.id);
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                        ],
+                      );
+                    },
+                  )
+                else
+                  const CircularProgressIndicator(),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => _carouselController.previousPage(),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.yellow.shade700,
+                      ),
+                      child: const Icon(Icons.arrow_back),
+                    ),
+                    const SizedBox(width: 20),
+                    ElevatedButton(
+                      onPressed: () => _carouselController.nextPage(),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.yellow.shade700,
+                      ),
+                      child: const Icon(Icons.arrow_forward),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
       drawer: const LeftDrawer(),
