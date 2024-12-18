c4ee8841d1da12b814aa8d91c01092488a286dbc
zerothy <zxuyagaming@gmail.com>
2024-12-18 14:13:42 +0700

    feat: add supabase

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,17 @@ import 'package:depokrasa_mobile/authentication/screens/login.dart';
 import 'package:provider/provider.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
+Future<void> main() async {
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+  await Supabase.initialize(
+    url: 'https://ibuqzrjrzshvnuahjjdm.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlidXF6cmpyenNodm51YWhqamRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzNjUxMTEsImV4cCI6MjA0OTk0MTExMX0.seInsPtndZA-mW2sqDakhTWlwanbhVfXVmWfdtk0ZUc',
+  );
+  runApp(MyApp());
 }
-
+        
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -29,3 +34,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
